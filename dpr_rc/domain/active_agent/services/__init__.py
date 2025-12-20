b1b0fd465f0d54e6450e5c935a932aa2d30f4b37
Alan Garcia <alan@pix.wine>
2025-12-20 14:29:57 -0600

    Fix ImportError: Export FoveatedRouter in services package

--- dpr_rc/domain/active_agent/services/__init__.py
+++ dpr_rc/domain/active_agent/services/__init__.py
@@ -5,10 +5,12 @@ Domain Services for Active Agent
 from .consensus_calculator import ConsensusCalculator, Vote
 from .routing_service import RoutingService
 from .response_synthesizer import ResponseSynthesizer
+from .foveated_router import FoveatedRouter
 
 __all__ = [
     "ConsensusCalculator",
     "Vote",
     "RoutingService",
     "ResponseSynthesizer",
+    "FoveatedRouter",
 ]
